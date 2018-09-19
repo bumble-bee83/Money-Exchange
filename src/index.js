ba18600fc05ad6cbc2d5f19f169b07e0d0439fbe
Litvinov Alexander <bumble-bee83@mail.ru>
2018-09-19 21:44:47 +0300

    Money-Exchange: exec task

--- src/index.js
+++ src/index.js
@@ -2,4 +2,27 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+    let errObject = {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    if (currency > 10000){
+        return errObject;
+    }
+
+    let resultObject = {};
+
+    function setResultObject(propertyName, coinValue, summValue){
+        let coinCount = Math.floor(summValue / coinValue);
+        if (coinCount != 0){
+            resultObject[propertyName] = coinCount;
+        }
+    }
+
+    if (currency > 0){
+        setResultObject('H', 50, currency);
+        setResultObject('Q', 25, currency % 50);
+        setResultObject('D', 10, currency % 50 % 25);
+        setResultObject('N', 5, currency % 50 % 25 % 10);
+        setResultObject('P', 1, currency % 50 % 25 % 10 % 5);
+    }
+
+    return resultObject;
 }
